0203f9f4fa5a4b3544adc622ed56b92d585d5641
Ingrid Sarap <ingrid.sarap@gmail.com>
2015-04-29 00:13:19 +0300

    parandus

--- kandideerimine.php
+++ kandideerimine.php
@@ -4,7 +4,7 @@
 
 <!DOCTYPE html>
 
-<html lang="en" manifest="manifest.appcache">
+<html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	    <meta charset="utf-8">
--- register-success.php
+++ register-success.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
-<html lang="en" manifest="manifest.appcache">
+<html lang="en">
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 	    <meta charset="utf-8">
