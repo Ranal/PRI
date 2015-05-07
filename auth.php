81afaf7861fcc099f2716d240b78b16561b76c6f
Ranal Saron <ranalsaron@gmail.com>
2015-05-07 15:03:43 +0300

    Added functionality 3.2
    Redirect to requested page after logging in

--- auth.php
+++ auth.php
@@ -1,11 +1,16 @@
 <?php
 	//Start session
 	session_start();
+    $_SESSION['url'] = $_SERVER['REQUEST_URI']; 
+
 	
 	//Check whether the session variable SESS_MEMBER_ID is present or not
 	if(!isset($_SESSION['SESS_MEMBER_ID']) || (trim($_SESSION['SESS_MEMBER_ID']) == '')) {
-		      header("Location:login-form.php?location=" . urlencode($_SERVER['REQUEST_URI']));
+              //header("location: access-denied.php");
+		      //header("Location:login-form.php?location=" . urlencode($_SERVER['REQUEST_URI']));
               // Note: $_SERVER['REQUEST_URI'] is your current page
+        
+             include 'login-form.php';
 		exit();
 	}
 ?>
